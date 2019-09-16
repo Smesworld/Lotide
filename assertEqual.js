33104e8c6d2615b286808fa5b46be181190b24e9
Alan Dodge <alphaechododge@gmail.com>
2019-09-16 21:42:47 +0000

    Added README and added and implemented assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎ Assertion Passed: " + actual + " === " + expected + " ✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎");
+  } else {
+    console.log("✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎ Assertion Failed: " + actual + " !== " + expected + " ✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎");
+  }
+};
+
+assertEqual("Whiskey", "Whiskey");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, -5);
+assertEqual(0, false);
