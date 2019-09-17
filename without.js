d5f75eb2cf75d414dbc46e00e3fc5226e621cf4c
Alan Dodge <alphaechododge@gmail.com>
2019-09-17 22:15:28 +0000

    Added check to ensure not changing original array

--- without.js
+++ without.js
@@ -34,3 +34,7 @@ assertArraysEqual(without([], [1]), []);
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 assertArraysEqual(without([1, 2, 3, 4, 5, "6", 4, 3, 2, 4, "e"], [2, 4, "e"]), [1, 3, 5, "6", 3]);
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
