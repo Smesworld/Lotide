3ab27a829d3518ea9579b77d7ce761ec8bb2ad77
Alan Dodge <alphaechododge@gmail.com>
2019-09-17 22:06:04 +0000

    Added without which removes all elements in a given array included in a second array

---
+++ without.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎ Assertion Passed: ${actual} === ${expected} ✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎`);
+  } else {
+    console.log(`✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎ Assertion Failed: ${actual} !== ${expected} ✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let product = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      product.push(source[i]);
+    }
+  }
+
+  return product;
+};
+
+assertArraysEqual(without([], [1]), []);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([1, 2, 3, 4, 5, "6", 4, 3, 2, 4, "e"], [2, 4, "e"]), [1, 3, 5, "6", 3]);
