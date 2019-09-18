bb79210167e66a1b3b0feaee45272829165747a8
Alan Dodge <alphaechododge@gmail.com>
2019-09-18 16:23:31 +0000

    Improved the way assertArraysEqual displays the compaired arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,31 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
+const eqArrays = function(firstArray, secondArray) {
+  let equal = true;
+  if (firstArray.length !== secondArray.length) {
+    equal = false;
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (Array.isArray(firstArray[i]) && Array.isArray(secondArray[i])) {
+      equal = eqArrays(firstArray[i], secondArray[i]);
+    } else if (firstArray[i] !== secondArray[i]) {
+      equal = false;
     }
   }
-  return true;
+
+  return equal;
 };
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎ Assertion Passed: ${actual} === ${expected} ✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎`);
+    console.log("✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎ Assertion Passed: ", actual, " === ", expected, " ✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎✔︎︎︎︎");
   } else {
-    console.log(`✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎ Assertion Failed: ${actual} !== ${expected} ✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎`);
+    console.log("✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎ Assertion Failed: ", actual, " !== ", expected, " ✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎✖︎︎︎︎");
   }
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1], [1, 2]);
 assertArraysEqual([1], ["1"]);
 assertArraysEqual([], []);
+assertArraysEqual([1, [2]], [1, [2]]);
+assertArraysEqual([1, [2]], [1, 2]);
+assertArraysEqual([[1, 2], [3, [4]]], [[1, 2], [3, [4]]]);
